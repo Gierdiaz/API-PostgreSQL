48790c6f00bf81f0ea9437d5dc304cef2aeadd48
gierdiaz <gierdiaz@hotmail.com>
2022-09-13 20:57:12 -0300

    api with postgresql

---
+++ db/connection.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"app/configs"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error){
+	conf := configs.GetDB()
+	
+	//string connection
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	conf.Host, conf.Port, conf.User, conf.Passs, conf.Database)
+
+	//connection
+	conn, err := sql.Open("postgre", sc)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
